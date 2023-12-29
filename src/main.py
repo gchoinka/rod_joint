7cfc549591a8d0466dc3638c38e5a2e160caf2b0
Gerard Choinka <gerard.choinka@gmail.com>
2023-12-29 23:31:01 +0000

    refactor common solid2 functions to module

--- src/main.py
+++ src/main.py
@@ -1,60 +1,38 @@
-import math  # noqa
-import multiprocessing
 import os
 import shutil
 import sys
+from typing import Iterable, Tuple, Dict, List
 
-from solid2 import *
+from solid2 import cube, linear_extrude, polygon, cylinder, hull, scale, intersection, P3
+from solid2.core.object_base import OpenSCADObject
+from solid2_utils.utils import save_to_str_scad, StlTask
 
-view_helper = 0.05
+verbose = False
+unprintable_thickness = 0.01
+preview_fix = 0.05
+
+pipe_r = 8 / 2
 
 
-def cube_example():
-    return cube()
-
-
-def task(stl_task):
-    obj, filename = stl_task
-    obj.save_as_stl(filename)
+def cube_example() -> Tuple[OpenSCADObject, P3]:
+    return cube(1), (0, 0, 0)
 
 
 def main(output_scad_basename, output_stl_basename):
-    output = [
+    output: List[StlTask] = [
         (cube_example(), "cube_example"),
     ]
-
-    stl_task = []
-    all_obj = None
-    obj_distance = 25
-    next_pos = [obj_distance, obj_distance]
-
-    for obj, filename_prefix in output:
-        filename = output_scad_basename + filename_prefix + ".scad"
-        stl_task.append((obj, output_scad_basename + filename_prefix + ".stl"))
-        obj.save_as_scad(filename)
-        if all_obj is None:
-            all_obj = obj
-        else:
-            all_obj += obj.left(next_pos[0]).fwd(next_pos[1])
-            next_pos[0] += obj_distance
-            next_pos[1] += obj_distance
-
-    filename = output_scad_basename + "all.scad"
-    all_obj.save_as_scad(filename)
-    stl_task.append((all_obj, output_scad_basename + "all.stl"))
-    if output_stl_basename is not None:
-        with multiprocessing.Pool() as pool:
-            pool.map(task, stl_task)
+    save_to_str_scad(output_scad_basename, output_stl_basename, output, verbose)
 
 
 if __name__ == "__main__":
-    skip_stl = True if len(sys.argv) > 1 and sys.argv[1] == "--fast" else False
-    build_path = os.path.dirname(os.path.realpath(__file__))
-    output_path = os.path.abspath(os.path.join(build_path, '..', 'build')) + os.path.sep
+    skip_stl: bool = True if len(sys.argv) > 1 and sys.argv[1] == "--fast" else False
+    build_path: str = os.path.dirname(os.path.realpath(__file__))
+    output_path: str = os.path.abspath(os.path.join(build_path, '..', 'build')) + os.path.sep
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    stl_output_path = output_path
+    stl_output_path: str | None = output_path
     if shutil.which("openscad") is None or skip_stl:
         stl_output_path = None
     main(output_scad_basename=output_path, output_stl_basename=stl_output_path)
